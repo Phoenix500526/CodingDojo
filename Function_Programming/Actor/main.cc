cec611367aa1bcf3c3f9c232eaa216b92fcbff61
Phoenix <Phoenix500526@163.com>
2021-02-23 21:32:54 +0800

    添加 Filter Actor 的实现

--- Function_Programming/Actor/main.cc
+++ Function_Programming/Actor/main.cc
@@ -1,10 +1,12 @@
 #include <boost/asio.hpp>
 #include <iostream>
 
+#include "filter.h"
 #include "service.h"
 #include "sink.h"
 #include "transform.h"
 #include "trim.h"
+#include "values.h"
 
 using namespace std;
 using namespace reactive::operators;
@@ -24,10 +26,16 @@ ostream& operator<<(std::ostream& out, const bookmark_t& page) {
 
 int main(int argc, char const* argv[]) {
     boost::asio::io_service event_loop;
+    std::vector<string> res;
     auto sink_to_cerr =
-        sink([](const auto& message) { std::cerr << message << '\n'; });
+        sink([&res](const auto& message) { std::cerr << message << '\n'; });
     // Starting the Boost.ASIO service
-    auto pipeline = service(event_loop) | transform(trim) | sink_to_cerr;
+    auto pipeline =
+        reactive::values<string>{"", "# Helo  ", "Halo ", "#  World"} |
+        filter([](const string& message) {
+            return message.length() > 0 && message[0] != '#';
+        }) |
+        transform(trim) | sink_to_cerr;
     cerr << "Service is running...\n";
     event_loop.run();
     return 0;
