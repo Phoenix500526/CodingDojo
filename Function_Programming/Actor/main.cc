ee0be794520c76e66224d8548fada2c209fcf3e7
Phoenix <Phoenix500526@163.com>
2021-02-25 22:33:28 +0800

    添加了对用户进行响应的实现
    在这个 web 服务当中，由于所有的消息都会在流水线上传递，因此需要一个额外的数据类型来保留
    client 的 socket 信息。我们引入了 with_client 数据结构来封装这一上下文(message + socket)
    显然，service 可以看成一个 monad，而 service 中的 with_client 也是一个 monad。

--- Function_Programming/Actor/main.cc
+++ Function_Programming/Actor/main.cc
@@ -1,23 +1,28 @@
 #include <boost/asio.hpp>
 #include <iostream>
+#include <nlohmann/json.hpp>
 
 #include "expected.h"
 #include "filter.h"
 #include "join.h"
+#include "mtry.h"
 #include "service.h"
 #include "sink.h"
 #include "transform.h"
 #include "trim.h"
 #include "values.h"
-#include "mtry.h"
 
 using namespace std;
 using namespace reactive::operators;
+using json = nlohmann::json;
 
 struct bookmark_t {
     string url;
     string text;
 };
+
+using expected_json = expected<json, std::exception_ptr>;
+using expected_bookmark = expected<bookmark_t, std::exception_ptr>;
 
 string to_string(const bookmark_t& page) {
     return "[" + page.text + "](" + page.url + ")";
@@ -27,33 +32,50 @@ ostream& operator<<(std::ostream& out, const bookmark_t& page) {
     return out << '[' << page.text << "](" << page.url << ")";
 }
 
+expected_bookmark bookmark_from_json(const json& data) {
+    return mtry([&] {
+        return bookmark_t{data.at("FirstURL"), data.at("Text")};
+    });
+}
+
 int main(int argc, char const* argv[]) {
     std::ios_base::sync_with_stdio(false);
     boost::asio::io_service event_loop;
-    auto sink_to_cerr =
-        sink([](const auto& message) { std::cerr << message << '\n'; });
+    auto transform = [](auto f) {
+        return reactive::operators::transform(lift_with_client(f));
+    };
+    auto filter = [](auto f) {
+        return reactive::operators::filter(apply_with_client(f));
+    };
     // Starting the Boost.ASIO service
-    auto pipeline = reactive::values<unsigned short>{10086, 10011, 10010} |
-                    transform([&event_loop](unsigned short port) {
-                        return service(event_loop, port);
-                    }) |
-                    join() | filter([](const string& message) {
-                        return message.length() > 0 && message[0] != '#';
-                    }) |
-                    transform(trim) | 
-                    transform([](const string& message){
-                        return mtry([&]{
-                            message.at(1);
-                            return message;
-                        });
-                    })  |
-                    sink([](const auto& exp_message){
-                        if(!exp_message){
-                            cerr << "ERROR: Request was not understood\n";
-                        }else{
-                            cerr << exp_message.get() << '\n';
-                        }
-                    });
+
+    auto pipeline =
+        reactive::values<unsigned short>{10086, 10011, 10010} |
+        reactive::operators::transform([&event_loop](unsigned short port) {
+            return service(event_loop, port);
+        }) |
+        join() | filter([](const string& message) {
+            return message.length() > 0 && message[0] != '#';
+        }) |
+        transform(trim) | transform([](const string& message) {
+            return mtry([&] { return json::parse(message); });
+        }) |
+        transform(
+            [](const auto& exp) { return mbind(exp, bookmark_from_json); }) |
+        sink([](const auto& message) {
+            const auto exp_bookmark = message.value;
+
+            if (!exp_bookmark) {
+                message.reply("ERROR: Request not understood\n");
+                return;
+            }
+
+            if (exp_bookmark->text.find("C++") != std::string::npos) {
+                message.reply("OK: " + to_string(exp_bookmark.get()) + "\n");
+            } else {
+                message.reply("ERROR: Not a C++-related link\n");
+            }
+        });
     cerr << "Service is running...\n";
     event_loop.run();
     return 0;
--- Function_Programming/Actor/service.h
+++ Function_Programming/Actor/service.h
@@ -5,6 +5,48 @@
 
 using boost::asio::ip::tcp;
 
+namespace detail {
+
+// with_client 是一个 monad
+template <class MessageType>
+struct with_client {
+    MessageType value;
+    tcp::socket* socket;
+
+    void reply(const std::string& message) const {
+        boost::asio::async_write(*socket,
+                                 boost::asio::buffer(message, message.length()),
+                                 [](auto, auto) {});
+    }
+};
+
+}  // namespace detail
+
+// make_with_client 是一个构造器
+template <class MessageType>
+auto make_with_client(MessageType&& value, tcp::socket* socket) {
+    return ::detail::with_client<MessageType>{std::forward<MessageType>(value),
+                                              socket};
+}
+
+// lift_with_client 可以将一个 f:T1 -> T2 的函数提升为 g:with_client<T1> ->
+// with_client<T2>, 相当于 fmap
+template <class F>
+auto lift_with_client(F&& function) {
+    return [function = std::forward<F>(function)](auto&& ws) {
+        return make_with_client(std::invoke(function, ws.value), ws.socket);
+    };
+}
+
+// apply_with_client 会将 with_client<T> 拆包，得到 T，并将函数 function 应用于
+// T 上
+template <class F>
+auto apply_with_client(F&& function) {
+    return [function = std::forward<F>(function)](auto&& ws) {
+        return std::invoke(function, std::forward<decltype(ws)>(ws).value);
+    };
+}
+
 // session 并不是 actor
 template <typename EmitFunction>
 class session : public std::enable_shared_from_this<session<EmitFunction>> {
@@ -27,7 +69,7 @@ private:
                     std::istream is(&m_data);
                     std::string line;
                     std::getline(is, line);
-                    m_emit(std::move(line));
+                    m_emit(make_with_client(std::move(line), &m_socket));
                     do_read();
                 }
             });
@@ -46,7 +88,7 @@ auto make_shared_session(Socket&& socket, EmitFunction&& emit) {
 
 class service {
 public:
-    using value_type = std::string;
+    using value_type = ::detail::with_client<std::string>;
     explicit service(boost::asio::io_service& service,
                      unsigned short port = 42042);
     service(const service&) = delete;
@@ -62,7 +104,7 @@ private:
     void do_accept();
     tcp::acceptor m_acceptor;
     tcp::socket m_socket;
-    std::function<void(std::string&&)> m_emit;
+    std::function<void(::detail::with_client<std::string>&&)> m_emit;
     friend std::ostream& operator<<(std::ostream& out, const service& service) {
         return out << "service object";
     }
