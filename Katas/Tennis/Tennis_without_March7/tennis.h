811715e117de97a0120125f66d7acf327dc97fc3
Phoenix <Phoenix500526@163.com>
2021-02-26 17:52:47 +0800

    添加 normal_scoring 模式匹配

--- Katas/Tennis/Tennis_without_March7/tennis.h
+++ Katas/Tennis/Tennis_without_March7/tennis.h
@@ -19,18 +19,63 @@ enum class player {
 struct normal_scoring {
     points player_1_points;
     points player_2_points;
+    inline bool operator==(const normal_scoring& other) const {
+        return player_1_points == other.player_1_points &&
+               player_2_points == other.player_2_points;
+    }
 };
 
 struct forty_scoring {
     player leading_player;
     points other_player_points;
+    inline bool operator==(const forty_scoring& other) const {
+        return leading_player == other.leading_player &&
+               other_player_points == other.other_player_points;
+    }
 };
 
 struct deuce {};
 
 struct advantage {
     player player_with_advantage;
+    inline bool operator==(const advantage& other) const {
+        return player_with_advantage == other.player_with_advantage;
+    }
 };
+
+template <typename E, E first, E head>
+void advanceEnum(E& v) {
+    if (v == head) v = first;
+}
+
+template <typename E, E first, E head, E next, E... tail>
+void advanceEnum(E& v) {
+    if (v == head)
+        v = next;
+    else
+        advanceEnum<E, first, next, tail...>(v);
+}
+
+template <typename E, E first, E... values>
+struct EnumValues {
+    static void advance(E& v) { advanceEnum<E, first, first, values...>(v); }
+};
+
+using points_values =
+    EnumValues<points, points::love, points::fifteen, points::thirty>;
+
+points& operator++(points& f) {
+    points_values::advance(f);
+    return f;
+}
+
+template <typename... Ts>
+struct overloaded : Ts... {
+    using Ts::operator()...;
+};
+
+template <typename... Ts>
+overloaded(Ts...) -> overloaded<Ts...>;
 
 }  // namespace detail
 
@@ -40,6 +85,43 @@ public:
         : m_state(detail::normal_scoring{detail::points::love,
                                          detail::points::love}) {}
     auto get_state() const { return m_state; }
+    auto point_for(detail::player which_player) {
+        std::visit(
+            detail::overloaded{
+                [&](detail::normal_scoring& state) {
+                    using namespace detail;
+                    switch (which_player) {
+                        case player::player_1:
+                            if (state.player_1_points != points::thirty) {
+                                ++state.player_1_points;
+                            } else {
+                                m_state = forty_scoring{player::player_1,
+                                                        state.player_2_points};
+                            }
+                            break;
+                        case player::player_2:
+                            if (state.player_2_points != points::thirty) {
+                                ++state.player_2_points;
+                            } else {
+                                m_state = forty_scoring{player::player_2,
+                                                        state.player_1_points};
+                            }
+                            break;
+                    }
+                },
+                [&](detail::forty_scoring& state) {
+
+                },
+                [&](detail::deuce& state) {
+
+                },
+                [&](detail::advantage& state) {
+
+                },
+            },
+            m_state);
+        return get_state();
+    }
 
 private:
     std::variant<detail::normal_scoring, detail::forty_scoring, detail::deuce,
--- Katas/Tennis/Tennis_without_March7/test/TennisTest.cc
+++ Katas/Tennis/Tennis_without_March7/test/TennisTest.cc
@@ -5,13 +5,90 @@
 #include "tennis.h"
 
 using namespace std;
+using namespace tennis::detail;
 
-TEST(TennisInitialStateTestCase, InitialStateShouldBeZeroToZero) {
-    using namespace tennis::detail;
+class TennisTest : public ::testing::Test {
+public:
+    TennisTest()
+        : love_love{points::love, points::love},
+          fifteen_fifteen{points::fifteen, points::fifteen},
+          thirty_thirty{points::thirty, points::thirty},
+          love_fifteen{points::love, points::fifteen},
+          fifteen_love{points::fifteen, points::love},
+          love_thirty{points::love, points::thirty},
+          thirty_love{points::thirty, points::love},
+          fifteen_thirty{points::fifteen, points::thirty},
+          thirty_fifteen{points::thirty, points::fifteen},
+          forty_love{player::player_1, points::love},
+          forty_fifteen{player::player_1, points::fifteen},
+          forty_thirty{player::player_1, points::thirty},
+          love_forty{player::player_2, points::love},
+          fifteen_forty{player::player_2, points::fifteen},
+          thirty_forty{player::player_2, points::thirty},
+          player_1_advantage{player::player_1},
+          player_2_advantage{player::player_2} {}
+
+protected:
+    normal_scoring love_love;
+    normal_scoring fifteen_fifteen;
+    normal_scoring thirty_thirty;
+    normal_scoring love_fifteen;
+    normal_scoring fifteen_love;
+    normal_scoring love_thirty;
+    normal_scoring thirty_love;
+    normal_scoring fifteen_thirty;
+    normal_scoring thirty_fifteen;
+
+    forty_scoring forty_love;
+    forty_scoring forty_fifteen;
+    forty_scoring forty_thirty;
+    forty_scoring love_forty;
+    forty_scoring fifteen_forty;
+    forty_scoring thirty_forty;
+
+    advantage player_1_advantage;
+    advantage player_2_advantage;
+};
+
+TEST_F(TennisTest, InitialStateShouldBeZeroToZero) {
     tennis::tennis_t test_1;
     auto test_1_state = test_1.get_state();
     EXPECT_EQ(std::holds_alternative<normal_scoring>(test_1_state), true);
-    auto* test_1_points = std::get_if<normal_scoring>(&test_1_state);
-    EXPECT_EQ(test_1_points->player_1_points, points::love);
-    EXPECT_EQ(test_1_points->player_2_points, points::love);
+    auto test_1_points = *std::get_if<normal_scoring>(&test_1_state);
+    EXPECT_EQ(test_1_points, love_love);
+}
+
+TEST_F(TennisTest, OneWinPointsAndUpdateHisStates) {
+    tennis::tennis_t test_2;
+    tennis::tennis_t test_3;
+
+    auto test_2_state = test_2.get_state();
+    auto test_3_state = test_3.get_state();
+
+    auto test_2_scoring_state = *std::get_if<normal_scoring>(&test_2_state);
+    auto test_3_scoring_state = *std::get_if<normal_scoring>(&test_3_state);
+    ASSERT_EQ(test_2_scoring_state, love_love);
+    ASSERT_EQ(test_3_scoring_state, love_love);
+
+    test_2_state = test_2.point_for(player::player_1);
+    EXPECT_TRUE(std::holds_alternative<normal_scoring>(test_2_state));
+    EXPECT_EQ(*(std::get_if<normal_scoring>(&test_2_state)), fifteen_love);
+
+    test_2_state = test_2.point_for(player::player_1);
+    EXPECT_EQ(*(std::get_if<normal_scoring>(&test_2_state)), thirty_love);
+
+    test_2_state = test_2.point_for(player::player_1);
+    EXPECT_TRUE(std::holds_alternative<forty_scoring>(test_2_state));
+    EXPECT_EQ(*(std::get_if<forty_scoring>(&test_2_state)), forty_love);
+
+    test_3_state = test_3.point_for(player::player_2);
+    EXPECT_TRUE(std::holds_alternative<normal_scoring>(test_3_state));
+    EXPECT_EQ(*(std::get_if<normal_scoring>(&test_3_state)), love_fifteen);
+
+    test_3_state = test_3.point_for(player::player_2);
+    EXPECT_EQ(*(std::get_if<normal_scoring>(&test_3_state)), love_thirty);
+
+    test_3_state = test_3.point_for(player::player_2);
+    EXPECT_TRUE(std::holds_alternative<forty_scoring>(test_3_state));
+    EXPECT_EQ(*(std::get_if<forty_scoring>(&test_3_state)), love_forty);
 }
