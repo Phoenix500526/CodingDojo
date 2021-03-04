3dc60efb452c5b74407c887cf451f02f497cd337
Phoenix <Phoenix500526@163.com>
2021-03-04 20:44:04 +0800

    完成 LeapYear Kata

---
+++ Katas/LeapYear/test/LeapYearTest.cc
@@ -0,0 +1,24 @@
+#include <gtest/gtest.h>
+
+#include "leapyear.h"
+
+using namespace std;
+
+TEST(LeapYearUnitTest, LeapYearCanBeDividedBy400){
+    ASSERT_TRUE(is_leap_year(400));
+    ASSERT_TRUE(is_leap_year(800));
+    ASSERT_TRUE(is_leap_year(1200));
+    ASSERT_FALSE(is_leap_year(100));
+    ASSERT_FALSE(is_leap_year(300));
+    ASSERT_FALSE(is_leap_year(500));
+}
+
+TEST(LeapYearUnitTest, LeapYearCanBeDividedBy4ButNot100){
+    ASSERT_TRUE(is_leap_year(2008));
+    ASSERT_TRUE(is_leap_year(2012));
+    ASSERT_TRUE(is_leap_year(2016));
+    ASSERT_FALSE(is_leap_year(2100));
+    ASSERT_FALSE(is_leap_year(2017));
+    ASSERT_FALSE(is_leap_year(2018));
+    ASSERT_FALSE(is_leap_year(1700));
+}
